c4fea32f4351d4a45f85acdffcdbf30b8228f9a5
bassel-mousa <--get>
2021-12-05 00:19:32 +0200

    Making Patient Structure

---
+++ database/migrations/2021_12_04_220140_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->integer('id_number');
+            $table->integer('phone_number')->unique();
+            $table->string('gender');
+            $table->date('date_of_birth');
+            $table->string('blood_group', 5);
+            $table->string('country');
+            $table->string('city');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
