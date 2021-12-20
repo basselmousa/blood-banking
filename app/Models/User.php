7f069181630c1acd962b8bcca8cbbd0f1653f111
bassel-mousa <--get>
2021-12-20 21:05:00 +0200

    Access Fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'full_name',
+        'phone_number'
     ];
 
     /**
