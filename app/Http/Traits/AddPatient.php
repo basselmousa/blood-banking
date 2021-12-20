52ec2cf841b48f24a0c0bd67bb0e7d56e81ef835
bassel-mousa <--get>
2021-12-20 21:03:33 +0200

    Making Add Trait To Clean the code

---
+++ app/Http/Traits/AddPatient.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Http\Traits;
+
+
+use App\Models\Donor;
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+
+trait AddPatient
+{
+    public function addPatient(Request $request, $type)
+    {
+        $request->validate([
+            'full_name' => ['required'],
+            'id_number' => ['required','unique:patients,id_number'],
+            'phone_number' => ['required','unique:patients,phone_number'],
+            'gender' => ['required','not_in:0'],
+            'birthday' => ['required'],
+            'blood' => ['required','not_in:0'],
+            'country' => ['required','not_in:0'],
+            'city' => ['required','not_in:0'],
+            'address' => ['required'],
+        ]);
+
+        Patient::create([
+            'type' => $type,
+            'full_name' => $request->full_name,
+            'id_number' => $request->id_number,
+            'phone_number' => $request->phone_number,
+            'gender' => $request->gender,
+            'date_of_birth' => $request->birthday,
+            'blood_group' => $request->blood,
+            'country' => $request->country,
+            'city' => $request->city,
+            'address' => $request->address,
+        ]);
+    }
+
+    public function addDonor(Request $request)
+    {
+        $request->validate([
+            'full_name' => ['required'],
+            'id_number' => ['required', 'unique:donors,id_number'],
+            'email' => ['required', 'unique:donors,email'],
+            'phone_number' => ['required', 'unique:donors,phone_number'],
+            'gender' => ['required','not_in:0'],
+            'birthday' => ['required'],
+            'blood' => ['required','not_in:0'],
+            'date' => ['required','before:today'],
+            'diseases' => ['required'],
+        ]);
+
+        Donor::create([
+            'full_name' => $request->full_name,
+            'id_number' => $request->id_number,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'gender' => $request->gender,
+            'date_of_birth' => $request->birthday,
+            'blood_group' => $request->blood,
+            'last_donation_date' => $request->date,
+            'diseases' => $request->diseases,
+        ]);
+    }
+}
